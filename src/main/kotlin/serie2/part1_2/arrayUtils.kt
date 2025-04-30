50b69fd0cf4b7a77df0e1ac5fdaf9ebfe0d6feec
alexz <alex.zaiets@hotmail.com>
2025-04-30 10:23:44 +0100

    fun minimum feita

--- src/main/kotlin/serie2/part1_2/arrayUtils.kt
+++ src/main/kotlin/serie2/part1_2/arrayUtils.kt
@@ -2,5 +2,20 @@ package serie2.part1_2
 import kotlin.random.Random
 
 fun minimum(maxHeap: Array<Int>, heapSize: Int): Int {
-    TODO("Not yet implemented")
+    val leaf = heapSize / 2
+    var min = maxHeap[leaf]
+
+    for(i in leaf until heapSize) {
+        if(maxHeap[i] < min) {
+            min = maxHeap[i]
+        }
+    }
+
+    return min
+}
+
+fun main() {
+    val heap = arrayOf(100, 50, 90, 20, 30, 80, 70)
+    val min = minimum(heap, heap.size)
+    print(min)
 }

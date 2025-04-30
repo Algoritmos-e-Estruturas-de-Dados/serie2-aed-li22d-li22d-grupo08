4eeb460419813009528d2701fcd0255006693f12
whitn <cassialawhitney@gmail.com>
2025-04-30 10:45:31 +0100

    .

--- src/main/kotlin/serie2/part1_2/IntArrayList.kt
+++ src/main/kotlin/serie2/part1_2/IntArrayList.kt
@@ -1,13 +1,25 @@
 package serie2.part1_2
 
-class IntArrayList : Iterable <Int> {
+class IntArrayList( ) : Iterable <Int> {
+    private val k=0
+    private val data= IntArray(k)
+    private var size = 0
+    private var rear=0
 
-    fun append(x:Int):Boolean {
-        TODO("Not yet implemented")
+
+    fun append(x: Int): Boolean {
+        if (size >= k) return false
+        data[rear] = x
+        rear = (rear + 1) % k
+        size++
+        return true
     }
 
+
     fun get(n:Int):Int?  {
-        TODO("Not yet implemented")
+
+
+
     }
 
     fun addToAll(x:Int)   {
